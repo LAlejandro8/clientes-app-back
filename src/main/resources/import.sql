8ab3658a38df7387485a12682fb34cbcebbf486f
Alejandro.Cortés <gibaleles@gmail.com>
2020-11-08 18:48:08 -0500

    Se implementa base para levantar servicio author:Me

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+/* Populate tabla clientes */
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Andrés', 'Guzmán', 'andres.guzman@email.com', '2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Sebastian', 'Hernandez', 'sebastian.hernandez@email.com', '2019-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Camilo', 'Cantillo', 'camilo.cantillo@email.com', '2020-01-01');
